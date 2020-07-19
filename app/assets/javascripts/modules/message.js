1b0e7104ce62c8dd63f74dd1bb5c527b6e961e06
yuktnk <yuki0wl@gmail.com>
2020-07-19 11:52:25 +0900

    edit-AutoUpdate

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="MessageBox">
+        `<div class="MessageBox" data-message-id=${message.id}>
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="MessageBox">
+      `<div class="MessageBox" data-message-id=${message.id}>
         <div class="MessageInfo">
           <div class="MessageInfo__userName">
             ${message.user_name}
@@ -58,9 +58,11 @@ $(function(){
       $('.Chat-main__message-list').append(html);
       $('.Chat-main__message-list').animate({ scrollTop: $('.Chat-main__message-list')[0].scrollHeight});
       $('form')[0].reset();
+      $('.Form__submit').prop("disabled", false);
     })
     .fail(function(){
       alert("メッセージ送信に失敗しました");
+      $('.Form__submit').prop("disabled", false);
     })
   });
 });
